42f37c540be6d9a9641494c77d432141f028d85f
Ariel <arielmontardo@gmail.com>
2018-09-24 19:21:38 -0300

    project mode changes

--- display_mux/src/main.c
+++ display_mux/src/main.c
@@ -1,11 +1,15 @@
 #define F_CPU 16000000UL
 #include <util/delay.h>
+#include <avr/io.h>
+
+#include "avr_gpio.h"
+#include "bits.h"
 
 int main(void)
 {
 
     while (1)
     {
-
+        
     }
 }
