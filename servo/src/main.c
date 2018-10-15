47656b5e3c4727af436e442ae6b7b8a5a229b2e6
Ariel <arielmontardo@gmail.com>
2018-10-15 15:51:37 -0300

    Cria projeto do servo

---
+++ servo/src/main.c
@@ -0,0 +1,21 @@
+/*
+ * main.c
+ *
+ * Created: Mon Oct 15 18:47:38 UTC 2018
+ * Author: ariel
+*/
+
+#include <avr/io.h>
+
+int main(void)
+{
+	// Program initialization
+	
+	while(1)
+	{
+		// Program loop
+		
+	}
+	// Should not reach
+	return 0;
+}
